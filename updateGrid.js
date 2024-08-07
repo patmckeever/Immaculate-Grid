cbdf28ff406f838c76251ce24f16a25ab38b219c
patmckeever <pmckeever@premierlacrosseleague.com>
2024-08-07 00:41:20 -0400

    new

--- updateGrid.js
+++ updateGrid.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 let json = require('./src/data.json');
 let data =  require('./src/players.json');
 
-const prevDate = '7/28/2024'
-const date = '7/29/2024'
+const prevDate = '8/13/2024'
+const date = '8/14/2024'
 
 let p = {};
 for (let name in data) {
