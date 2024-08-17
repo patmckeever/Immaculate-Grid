74ce7cb93c88474520db738e44105348c84ff563
patmckeever <pmckeever@premierlacrosseleague.com>
2024-08-17 02:39:00 -0400

    new

--- src/Daily.js
+++ src/Daily.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Box, Container, Typography, Button, CssBaseline, Paper, ClickAwayListener } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import image from './pll.png'; // Import the image
-import data from '../src/players.json';
+import data from '../src/final.json';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import styled from 'styled-components';
@@ -38,19 +38,19 @@ for (let name in data) {
 let count = 0;
 
 const category = {
-  "Waterdogs": {
+  "WAT": {
       text: "Waterdogs",
       image: require('./waterdogs.png'),
       description: "Waterdogs LC (2020-2023), Philadelphia Waterdogs (2024-Present)"
 
   },
-  "Atlas": {
+  "ATL": {
     text: "Atlas",
     image: require('./atlas.png'),
     description: "Atlas LC (2019-2023), New York Atlas (2024-Present)"
 
   },
-  "Archers": {
+  "ARC": {
     text: "Archers",
     image: require('./archers.png'),
     description: "Archers LC (2019-2023), Utah Archers (2024-Present)"
@@ -62,109 +62,109 @@ const category = {
     description: "Boston Cannons (MLL) (2001-2020), Cannons LC (2021-2023), Boston Cannons (2024-Present)"
 
   },
-  "Chaos": {
+  "CHA": {
     text: "Chaos",
     image: require('./chaos.png'),
     description: "Chaos LC (2019-2023), Carolina Chaos (2024-Present)"
 
   },
-  "Outlaws": {
+  "OUT": {
     text: "Outlaws",
     image: require('./outlaws.png'),
     description: "Denver Outlaws (MLL) (2006-2020), Denver Outlaws (2024-Present)"
 
   },
-  "Redwoods": {
+  "RED": {
     text: "Redwoods",
     image: require('./redwoods.png'),
     description: "Redwoods LC (2019-2023), California Redwoods (2024-Present)"
 
   },
-  "Whipsnakes": {
+  "WHP": {
     text: "Whipsnakes",
     image: require('./whipsnakes.png'),
     description: "Whipsnakes LC (2019-2023), Maryland Whipsnakes (2024-Present)"
 
   },
-  "Bayhawks": {
+  "BAY": {
     text: "Bayhawks",
     image: require('./Bayhawks.png'),
     description: "Baltimore Bayhawks (MLL) (2001-2006), Washington Bayhawks (MLL) (2007-2009), Chesapeake Bayhawks (MLL) (2010-2010)"
 
   },
-  "Dragons": {
+  "DRG": {
     text: "Dragons",
     image: require('./Dragons.png'),
     description: "San Francisco Dragons (MLL) (2006-2008)"
 
   },
-  "Riptide": {
+  "RIP": {
     text: "Riptide",
     image: require('./Riptide.png'),
     description: "Los Angeles Riptide (MLL) (2006-2008)"
 
   },
-  "Lizards": {
+  "NYL": {
     text: "Lizards",
     image: require('./Lizards.png'),
     description: "Long Island Lizards (MLL) (2001-2012), New York Lizards (MLL) (2013-2020)"
 
   },
-  "Hounds": {
+  "HND": {
     text: "Hounds",
     image: require('./Hounds.png'),
     description: "Charlotte Hounds (MLL) (2012-2018)"
 
   },
-  "Pride": {
+  "NJP": {
     text: "Pride",
     image: require('./Pride.png'),
     description: "New Jersey Pride (MLL) (2001-2008)"
 
   },
-  "Rattlers": {
+  "RAT": {
     text: "Rattlers",
     image: require('./Rattlers.png'),
     description: "Rochester Rattlers (MLL) (2001-2008, 2011-2017), Dallas Rattlers (MLL) (2018-2019)"
 
   },
-  "Launch": {
+  "LAU": {
     text: "Launch",
     image: require('./Launch.png'),
     description: "Florida Launch (MLL) (2014-2018)"
 
   },
-  "Blaze": {
+  "BLZ": {
     text: "Blaze",
     image: require('./Blaze.png'),
     description: "Atlanta Blaze (MLL) (2016-2019)"
 
   },
-  "Machine": {
+  "MAC": {
     text: "Machine",
     image: require('./Machine.png'),
     description: "Chicago Machine (MLL) (2006-2010), Ohio Machine (MLL) (2012-2018)"
 
   },
-  "Nationals": {
+  "NAT": {
     text: "Nationals",
     image: require('./Nationals.png'),
     description: "Toronto Nationals (MLL) (2009-2010), Hamilton Nationals (MLL) (2011-2013)"
 
   },
-  "Hammerheads": {
+  "HAM": {
     text: "Hammerheads",
     image: require('./Hammerheads.png'),
     description: "Connecticut Hammerheads (MLL) (2020)"
 
   },
-  "Chrome": {
+  "CHR": {
     text: "Chrome",
     image: require('./Chrome.png'),
     description: "Chrome LC (2019-2023)"
 
   },
-  "Barrage": {
+  "BAR": {
     text: "Barrage",
     image: require('./Barrage.png'),
     description: "Bridgeport Barrage (MLL) (2001-2003), Philadelphia Barrage (MLL) (2004-2008, 2020)"
@@ -228,44 +228,248 @@ const category = {
     text: "Syracuse",
     image: require('./Syracuse.png'),
     description: "Player played at Syracuse University"
-
   },
-  "30P": {
+  "North Carolina": {
+    text: "North Carolina",
+    image: require('./NorthCarolina.png'),
+    description: "Player played at the University of North Carolina"
+  },
+  "Ohio State": {
+    text: "Ohio State",
+    image: require('./OhioState.png'),
+    description: "Player played at Ohio State University"
+  },
+  "Penn State": {
+    text: "Penn State",
+    image: require('./PennState.png'),
+    description: "Player played at Penn State University"
+  },
+  "Michigan": {
+    text: "Michigan",
+    image: require('./Michigan.png'),
+    description: "Player played at the University of Michigan"
+  },
+  "Rutgers": {
+    text: "Rutgers",
+    image: require('./Rutgers.png'),
+    description: "Player played at Rutgers University"
+  },
+  "Yale": {
+    text: "Yale",
+    image: require('./Yale.png'),
+    description: "Player played at Yale University"
+  },
+  "Princeton": {
+    text: "Princeton",
+    image: require('./Princeton.png'),
+    description: "Player played at Princeton University"
+  },
+  "Penn": {
+    text: "Penn",
+    image: require('./Penn.png'),
+    description: "Player played at the University of Pennsylvania"
+  },
+  "Cornell": {
+    text: "Cornell",
+    image: require('./Cornell.png'),
+    description: "Player played at Cornell University"
+  },
+  "Brown": {
+    text: "Brown",
+    image: require('./Brown.png'),
+    description: "Player played at Brown University"
+  },
+  "Albany": {
+    text: "Albany",
+    image: require('./Albany.png'),
+    description: "Player played at the University of Albany"
+  },
+  "Villanova": {
+    text: "Villanova",
+    image: require('./Villanova.png'),
+    description: "Player played at Villanova University"
+  },
+  "Georgetown": {
+    text: "Georgetown",
+    image: require('./Georgetown.png'),
+    description: "Player played at Georgetown University"
+  },
+  "Marquette": {
+    text: "Marquette",
+    image: require('./Marquette.png'),
+    description: "Player played at Marquette University"
+  },
+  "High Point": {
+    text: "High Point",
+    image: require('./HighPoint.png'),
+    description: "Player played at High Point University"
+  },
+  "UMass": {
+    text: "UMass",
+    image: require('./UMass.png'),
+    description: "Player played at the University of Massachusetts"
+  },
+  "Towson": {
+    text: "Towson",
+    image: require('./Towson.png'),
+    description: "Player played at Towson University"
+  },
+  "Delaware": {
+    text: "Delaware",
+    image: require('./Delaware.png'),
+    description: "Player played at the University of Delaware"
+  },
+  "Hofstra": {
+    text: "Hofstra",
+    image: require('./Hofstra.png'),
+    description: "Player played at Hofstra University"
+  },
+  "Lehigh": {
+    text: "Lehigh",
+    image: require('./Lehigh.png'),
+    description: "Player played at Lehigh University"
+  },
+  "Loyola": {
+    text: "Loyola",
+    image: require('./Loyola.png'),
+    description: "Player played at Loyola University"
+  },
+  "undrafted": {
+    text: "UNDRAFTED",
+    image: "",
+    description: "Player did not get drafted"
+  },
+  "top20": {
+    text: "TOP 20 DRAFT PICK",
+    image: "",
+    description: "Player was drafted in the top 20"
+  },
+  "top10": {
+    text: "TOP 10 DRAFT PICK",
+    image: "",
+    description: "Player was drafted in the top 10"
+  },
+  "top5": {
+    text: "TOP 5 DRAFT PICK",
+    image: "",
+    description: "Player was drafted in the top 5"
+  },
+  "top1": {
+    text: "#1 DRAFT PICK",
+    image: "",
+    description: "Player was drafted #1 overall"
+  },
+  "star": {
+    text: "ALL-STAR",
+    image: "",
+    description: "Player has made an All-Star team in his career"
+  },
+  "star5": {
+    text: "5x ALL-STAR",
+    image: "",
+    description: "Player has made 5 or more All-Star teams in his career"
+  },
+  "champ": {
+    text: "CHAMP",
+    image: "",
+    description: "Player has won a PLL or MLL championship"
+  },
+  "mvp": {
+    text: "MVP",
+    image: "",
+    description: "Player has won an MVP award"
+  },
+  "allpro": {
+    text: "ALL-PRO",
+    image: "",
+    description: "Player has made a 1st or 2nd All-Pro team"
+  },
+  "opoy": {
+    text: "OPOY",
+    image: "",
+    description: "Player has won an offensive player of the year award"
+  },
+  "dpoy": {
+    text: "DPOY",
+    image: "",
+    description: "Player has won a defensive player of the year award"
+  },
+  "roy": {
+    text: "ROY",
+    image: "",
+    description: "Player has won a rookie of the year award"
+  },
+  "seasonPoints": {
     text: "30 P SEASON",
     image: "",
     description: "Player has scored 30 points in a single season. When paired with a team, does not need to be in the same season"
-
   },
-  "200P": {
+  "careerPoints": {
     text: "200 P CAREER",
     image: "",
     description: "Player has scored 200 points in his career"
-
   },
-  "30GB": {
+  "seasonGoals": {
+    text: "20 G SEASON",
+    image: "",
+    description: "Player has scored 20 goals in a single season. When paired with a team, does not need to be in the same season"
+  },
+  "careerGoals": {
+    text: "100 G CAREER",
+    image: "",
+    description: "Player has scored 100 goals in his career"
+  },
+  "seasonAssists": {
+    text: "20 A SEASON",
+    image: "",
+    description: "Player has record 20 assists in a single season. When paired with a team, does not need to be in the same season"
+  },
+  "careerAssists": {
+    text: "100 A CAREER",
+    image: "",
+    description: "Player has record 100 assists in his career"
+  },
+  "seasonGB": {
     text: "30 GB SEASON",
     image: "",
     description: "Player has recorded 30 groundballs in a single season. When paired with a team, does not need to be in the same season"
 
   },
-  "300GB": {
+  "careerGB": {
     text: "300 GB CAREER",
     image: "",
     description: "Player has recorded 300 groundballs in his career"
 
   },
-
-  "100SV": {
+  "seasonSaves": {
     text: "100 SAVE SEASON",
     image: "",
     description: "Player has recorded 100 saves in a single season. When paired with a team, does not need to be in the same season"
-
   },
-  "500SV": {
+  "careerSaves": {
     text: "500 SAVE CAREER",
     image: "",
     description: "Player has recorded 500 saves in his career"
-
+  },
+  "seasonCT": {
+    text: "15 CT SEASON",
+    image: "",
+    description: "Player has recorded 15 caused turnovers in a single season. When paired with a team, does not need to be in the same season"
+  },
+  "careerCT": {
+    text: "50 CT CAREER",
+    image: "",
+    description: "Player has recorded 50 caused turnovers in his career"
+  },
+  "careerGames": {
+    text: "100 GAME CAREER",
+    image: "",
+    description: "Player has played in 100 games in his career"
+  },
+  "careerTwos": {
+    text: "10 2G CAREER",
+    image: "",
+    description: "Player has recorded 10 two point goals in his career"
   },
   "TALL": {
     text: "6'3\" OR TALLER",
@@ -406,7 +610,7 @@ function Daily() {
   
   const fetchData = async () => {
     try {
-      const snapshot = await firestore.collection('players').get();
+      const snapshot = await firestore.collection('newPlayer').get();
       const fetchedData = {};
       snapshot.forEach(doc => {
         fetchedData[doc.id] = {
@@ -420,7 +624,7 @@ function Daily() {
   };   
   const fetchCategoryData = async () => {
     try {
-      const snapshot = await firestore.collection('categories').get();
+      const snapshot = await firestore.collection('newCategories').get();
       const fetchedData = {};
       snapshot.forEach(doc => {
         fetchedData[doc.id] = {
@@ -449,7 +653,7 @@ function Daily() {
 
   const updateData = async (PlayerName, newPlayerData) => {
     try {
-      await firestore.collection('players').doc(PlayerName).update(newPlayerData);
+      await firestore.collection('newPlayer').doc(PlayerName).update(newPlayerData);
       console.log('Data updated successfully');
       // Optionally, fetch updated data again
       fetchData();
@@ -459,7 +663,7 @@ function Daily() {
   };
   const updateCategoryData = async (CategoryName, newCategoryData) => {
     try {
-      await firestore.collection('categories').doc(CategoryName).set(newCategoryData, { merge: true });
+      await firestore.collection('newCategories').doc(CategoryName).set(newCategoryData, { merge: true });
       console.log('Data updated or added successfully');
       // Optionally, fetch updated data again
       fetchCategoryData();
@@ -645,10 +849,9 @@ function Daily() {
 
     for (let name in data) {
       const link = 'https://premierlacrosseleague.com/player/' + name.replace(" ","-");
-      const info = [data[name].team, data[name].year, data[name].sp, data[name].cp, data[name].sgb, data[name].cgb, data[name].ssv, data[name].csv, data[name].one, data[name].draft, data[name].tall, data[name].short, data[name].lefty, data[name].position, data[name].college, data[name].country, data[name].picture,link];
+      const info = [data[name].team, data[name].year, data[name].seasonPoints, data[name].careerPoints, data[name].seasonGoals, data[name].careerGoals,data[name].seasonGB,data[name].careerGB,data[name].seasonCT,data[name].careerCT,data[name].seasonSaves, data[name].careerSaves, data[name].careerGames, data[name].careerTwos ,data[name].seasonAssists, data[name].careerAssists,data[name].profileUrl, link, data[name].undrafted, data[name].top20, data[name].top10, data[name].top5,data[name].top1, data[name].star,data[name].star5,data[name].mvp,data[name].champ,data[name].allpro,data[name].opoy,data[name].dpoy,data[name].roy,data[name].one, data[name].tall, data[name].short, data[name].handedness, data[name].position, data[name].college, data[name].countryCode];
       p[name] = info;
     }
-   //console.log(p)
     setPlayers(p)
 
     generate(p);
@@ -725,19 +928,24 @@ function Daily() {
       newScore += 1
       jawn[selectedBox] = 1
       setCheckArray(jawn)
-      
+
+      console.log('here1')
       if(left[Math.floor(selectedBox/4)-1].localeCompare(top[(selectedBox%4)-1]) < 0) key = left[Math.floor(selectedBox/4)-1] + top[(selectedBox%4)-1]
       else key = top[(selectedBox%4)-1] + left[Math.floor(selectedBox/4)-1]
       
       if(CategoryData[key] == undefined) CategoryData[key] = {"total": 1}
       else CategoryData[key]["total"] += 1
+      console.log('here2')
 
+      console.log(PlayerData)
       if(PlayerData[playerName[selectedBox]].count[key] == undefined) PlayerData[playerName[selectedBox]].count[key] = 1
       else PlayerData[playerName[selectedBox]].count[key]++
+      console.log('here3')
 
       updateData(playerName[selectedBox],{ count: PlayerData[playerName[selectedBox]].count })
       updateCategoryData(key,CategoryData[key])
-      
+      console.log('here4')
+
       setRarity(rarity - (100-(Math.round(((PlayerData[playerName[selectedBox]].count[key]/CategoryData[key]['total'])*1000))/10)))
       newRarity = rarity - (100-(Math.round(((PlayerData[playerName[selectedBox]].count[key]/CategoryData[key]['total'])*1000))/10))
 
@@ -1257,7 +1465,7 @@ function Daily() {
 
             <Typography fontSize='12px' display = 'flex' justify-content = 'center' align-items = 'center'>Tap on a logo or category for help</Typography>
             <Typography fontSize='12px' marginTop="-5px">Important:</Typography>
-            <Typography fontSize='10px' marginTop="-15px">*Data from 2019 MLL season is currently unavailable<br></br>*MLL-only players don't have college/position/country/height data<br></br>*Player must have finished a season with MLL team to qualify<br></br>*Player must have finished NCAA career with team to qualify</Typography>
+            <Typography fontSize='10px' marginTop="-15px">*Player must have played a game with PLL/MLL team to qualify<br></br>*MLL-only players don't have college/country/height data<br></br>*Player must have finished NCAA career with team to qualify</Typography>
     
 
           </Box>
